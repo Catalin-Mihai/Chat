20ca414becd32438f3502e6958986470daf4170c
Catalin Mihai <catalinnm99@gmail.com>
2019-12-08 21:50:16 +0200

    gradle things

---
+++ app/src/main/java/com/example/catalin/chat/DataBaseQuery.java
@@ -0,0 +1,56 @@
+package com.example.catalin.chat;
+/*
+import android.provider.ContactsContract;
+import android.widget.BaseAdapter;
+
+public class DataBaseQuery {
+
+    private static DataBaseQuery instance = null;
+    private GlobalGetter globalGetter;
+
+    enum Interfaces {
+
+        interface I_GetUserByUserId
+        {
+            void UserReceived();
+            void InvalidId();
+        }
+    }
+
+
+    DataBaseQuery()
+    {
+        globalGetter = new GlobalGetter();
+        globalGetter.setListener(new GlobalListener());
+    }
+
+    public static DataBaseQuery getInstance() {
+        return instance;
+    }
+
+    User GetUserByUserId(int userid)
+    {
+
+        User user = new User();
+        user.setID(userid);
+        return user;
+    }
+
+    class GlobalListener implements GlobalGetter.HttpContentListener
+    {
+
+        GlobalListener()
+
+        @Override
+        public void ContentReceived(String response) {
+
+            switch ()
+        }
+
+        @Override
+        public void ContentNotFound() {
+
+        }
+    }
+}
+*/
